eb689801c227d87edf0506a956a3007fe0fc3e63
Zhou <theodoruszq@gmail.com>
2020-01-09 19:09:27 +0800

    The very first version

---
+++ README.txt
@@ -0,0 +1,14 @@
+This package is built for common python usage, and I will update for a long time.
+
+# How to upload to pypi?
+# https://juejin.im/post/5d370d94f265da1b8b2b9f71
+
+# 1. Build the package
+# pip install --user --upgrade setuptools wheel
+python3 setup.py sdist bdist_wheel
+
+# 2. Upload to website
+# pip install --user --upgrade twine
+# python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python -m twine upload dist/*
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zqlib',
+    version='0.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Common used package of Qiang Zhou',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy', 'opencv-python'],
+    url='https://github.com/BillMills/python-package-example',
+    author='Qiang Zhou',
+    author_email='theodoruszq@gmail.com'
+)
