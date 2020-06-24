a0ff8e4befb0f1f4878a5c26fbe704c555970453
zhouqiang.madamada <zhouqiang.madamada@bytedance.com>
2020-06-24 14:51:47 +0800

    update assemble function

--- zqlib/functions.py
+++ zqlib/functions.py
@@ -40,12 +40,27 @@ dumpimgs = lambda x, p: [cv2.imwrite("debug/{}_{:05d}.png".format(p, i), xi) for
 # 2020-01-02: Read txt files into a list
 readvdnames = lambda x: open(x).read().rstrip().split('\n')
 
-# 2020-01-10: Assemble multiple images into one
-def assemble_multiple_images(images, number_width=8, pad_index=True):
+# 2020-06-19: Add support for grey image
+# 2020-06-04: Update tag
+def assemble_multiple_images(images, number_width=8, tags=None):
     import math
-    images = np.asarray(images, dtype=np.uint8) # TxHxW for now
-    img_h, img_w = images.shape[1:3]
+    def grey2rgb(img):
+        if len(img.shape) == 2:
+            rgb = np.stack((img,)*3, axis=-1)
+        elif len(img.shape) == 3 and img.shape[2] == 1:
+            rgb = np.repeat(img, (1, 1, 3))
+        
+        if abs(np.max(img)-1) < 1e-2:
+            rgb = rgb * 255
+        return rgb
 
+    for i, image in enumerate(images):
+        if len(image.shape) == 2 or image.shape[2] == 1:
+            images[i] = grey2rgb(image)
+
+    images = np.asarray(images, dtype=np.uint8) # TxHxWxC for now
+    img_h, img_w = images.shape[1:3]
+    
     if len(images) % number_width != 0:
         number_mod = number_width - len(images) % number_width
         ph_images = np.zeros((number_mod, img_h, img_w))
@@ -53,19 +68,18 @@ def assemble_multiple_images(images, number_width=8, pad_index=True):
     number_images = len(images)
     number_group = int(number_images/number_width)
 
-    if pad_index:
-        for i in range(number_images):
-            images[i] = cv2.putText(images[i],
-                                "{:03d}".format(i),
-                                (20, img_h-20),
-                                cv2.FONT_HERSHEY_PLAIN,
-                                2, 255, thickness=2)
+    if tags is not None:
+        for i, tag in enumerate(tags):
+            images[i] = cv2.putText(images[i], tag, (20, img_h-20), 
+                                    cv2.FONT_HERSHEY_PLAIN,
+                                    fontScale=2, color=(0, 0, 255), thickness=2)
 
     group_images = []
+
     for g in range(number_group):
         group_image = images[g*number_width:(g+1)*number_width]
-        group_image = group_image.transpose((1, 0, 2))
-        group_image = group_image.reshape((img_h, number_width*img_w))
+        group_image = group_image.transpose((1, 0, 2, 3))
+        group_image = group_image.reshape((img_h, number_width*img_w, -1))
         group_images.append(group_image)
     group_images = np.concatenate(group_images, axis=0)
 
